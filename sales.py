159ec256a8fa811a513fec544ceb54a98ae3e6c0
jairH3110 <chiefeon3110@gmail.com>
2023-03-09 15:51:40 -0600

    firts commit

---
+++ sales.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import codecs
+
+
+st.title('My first app')
+st.header('My first app')
+st.write("""
+# Ejemplo Simple de una App de
+Predicción
+Esta app predice mis datos!
+""")
